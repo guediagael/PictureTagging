a919a0c8901cf848f82ecba8f7a30afcb9b1b024
guediagael <e.guedia@innopolis.ru>
2017-08-25 03:24:56 +0300

    added a 40%  black background with rounded corners

--- app/src/main/java/training/personal/picturetagging/TagView.java
+++ app/src/main/java/training/personal/picturetagging/TagView.java
@@ -1,6 +1,8 @@
 package training.personal.picturetagging;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,14 +16,21 @@ public class TagView extends RelativeLayout {
 
     public TagView(Context context,int x, int y,String tagText) {
         super(context);
-        mTagText = (tagText.trim().length()>10)?tagText.substring(0,9) : tagText;
+//        mTagText = (tagText.trim().length()>10)?tagText.substring(0,9) : tagText;
+        mTagText = tagText;
         TextView textView = new TextView(context);
         textView.setText(mTagText);
-        RelativeLayout.LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+        textView.setPadding(1,1,1,1);
+        textView.setTextColor(Color.WHITE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textView.setBackground(context.getDrawable(R.drawable.gradient));
+        }else textView.setBackground(context.getResources().getDrawable(R.drawable.gradient));
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         setPadding(x,y,0,0);
         setLayoutParams(params);
         addView(textView);
+
 
     }
 
